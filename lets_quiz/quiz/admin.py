2872802c818193dfcf69a1896b54114269001f60
akashgiricse <akashgiricse@gmail.com>
2018-02-22 16:16:54 +0530

    Made Choices appear with corresponding questions inline in admin panel

--- lets_quiz/quiz/admin.py
+++ lets_quiz/quiz/admin.py
@@ -4,5 +4,15 @@ from .models import Question, Choice
 
 # Register your models here.
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    model = Question
+    inlines = (ChoiceInline, )
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
