ef4256fc3cc61c6f0ba4157137467487b4375df5
akashgiricse <akashgiricse@gmail.com>
2018-02-25 00:31:38 +0530

    Display choices in template

--- lets_quiz/quiz/views.py
+++ lets_quiz/quiz/views.py
@@ -1,7 +1,8 @@
-import random
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import QuizProfile
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
+from .models import QuizProfile, Question
 # Create your views here.
 
 
@@ -13,9 +14,24 @@ def home(request):
 @login_required
 def play(request):
     quiz_profile, created = QuizProfile.objects.get_or_create(user=request.user)
-    question = quiz_profile.get_new_question()
 
-    context = {
-        'question': question,
-    }
-    return render(request, 'quiz/play.html', context=context)
+    if request.method == 'POST':
+        question_pk = request.POST.get('question_pk')
+        question = get_object_or_404(Question, question_pk)
+        choice_pk = request.POST.get('choice_pk')
+
+        try:
+            selected_choice = question.choices.get(pk=choice_pk)
+        except ObjectDoesNotExist:
+            raise Http404
+
+        if selected_choice.is_correct:
+            pass
+    else:
+        question = quiz_profile.get_new_question()
+
+        context = {
+            'question': question,
+        }
+
+        return render(request, 'quiz/play.html', context=context)
