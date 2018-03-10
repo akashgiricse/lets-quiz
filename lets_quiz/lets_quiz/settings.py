92f69f2bdb53aa58ce99e0936bc0e9c19eb2b915
akashgiricse <akashgiricse@gmail.com>
2018-03-10 13:07:03 +0530

    Set debug False

--- lets_quiz/lets_quiz/settings.py
+++ lets_quiz/lets_quiz/settings.py
@@ -25,7 +25,7 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 SECRET_KEY = 'tbdy@fqf+)_@_nv71f@%8rqh02s%d@_l@_iemu35@^*r_00m9l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
