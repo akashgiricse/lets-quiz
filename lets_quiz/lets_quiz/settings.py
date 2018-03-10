4584d5055c9030f3f5e0827f6b7da5f96ae96c32
akashgiricse <akashgiricse@gmail.com>
2018-03-10 14:11:13 +0530

    Remove load static from template

--- lets_quiz/lets_quiz/settings.py
+++ lets_quiz/lets_quiz/settings.py
@@ -122,10 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 
