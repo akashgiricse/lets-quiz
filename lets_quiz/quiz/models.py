89ab4c8bd69acfccc22eebe9972189ade46dfa5a
akashgiricse <akashgiricse@gmail.com>
2018-03-15 11:17:07 +0530

    Define Profile Model

--- lets_quiz/quiz/models.py
+++ lets_quiz/quiz/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import gettext as _
 from model_utils.models import TimeStampedModel
+from django.conf import settings
 
 
 class Question(TimeStampedModel):
@@ -73,3 +74,12 @@ class AttemptedQuestion(TimeStampedModel):
 
     def get_absolute_url(self):
         return f'/submission-result/{self.pk}/'
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    date_of_birth = models.DateField(blank=True, null=True)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
+
+    def __str__(self):
+        return 'Profile for user {}'.format(self.user.username)
