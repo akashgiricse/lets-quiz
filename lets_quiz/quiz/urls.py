7ca96b43ef1b358aef61df606b396d8aad597a46
akashgiricse <akashgiricse@gmail.com>
2018-03-19 10:54:49 +0530

    Add url for user edit form

--- lets_quiz/quiz/urls.py
+++ lets_quiz/quiz/urls.py
@@ -12,5 +12,7 @@ urlpatterns = [
     url(r'^login/', views.login_view, name='login'),
     url(r'^logout/', views.logout_view, name='logout'),
     url(r'^register/', views.register, name='register'),
+    url(r'^edit/', views.edit, name='edit'),
+
 
 ]
